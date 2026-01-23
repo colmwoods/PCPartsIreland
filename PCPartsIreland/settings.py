0077d517ed57e30b5f0e4f727bcfb24f54eb53f1
Colm Woods <colmwoods13@gmail.com>
2026-01-23 11:01:43 +0000

    Add import dj_database_url and change DB field on settings.py so can comuncate with Postgre DB

--- PCPartsIreland/settings.py
+++ PCPartsIreland/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Import environment variables from env.py if it exists
@@ -81,10 +82,7 @@ WSGI_APPLICATION = 'PCPartsIreland.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
