5474c44e5e28b9c4fb92c0563294745e01f78d3b
Colm Woods <colmwoods13@gmail.com>
2026-01-23 10:58:20 +0000

    Add both dj-database-url psycopg2-binary to requirements.txt file

--- PCPartsIreland/settings.py
+++ PCPartsIreland/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+# Import environment variables from env.py if it exists
+if os.path.isfile("env.py"):
+    import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1@ijwmf8y4k91mcz9ugmh4bmqw+^5)0hv(con7)6w91&^n1etg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
