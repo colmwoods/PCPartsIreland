eda6511f8aa940da0be02c7f0fa201bd6f59fef7
Colm Woods <colmwoods13@gmail.com>
2026-01-23 15:38:18 +0000

    Changed Debug from true to false

--- PCPartsIreland/settings.py
+++ PCPartsIreland/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com", "pcpartsireland.com", "www.pcpartsireland.com"]
 
 
