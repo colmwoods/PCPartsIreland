11a9656ae4cd4a2f8cd0b3e800a4d977e5d5b13d
Colm Woods <colmwoods13@gmail.com>
2026-01-23 10:32:57 +0000

    Change middleware.ts file to allow development server to work

--- PCPartsIreland/middleware.py
+++ PCPartsIreland/middleware.py
@@ -6,10 +6,15 @@ class CanonicalDomainMiddleware:
     Redirects any non-canonical host (including *.herokuapp.com and the apex)
     to the canonical domain (e.g. www.pcpartsireland.com).
     """
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        # Don't interfere with local development
+        if settings.DEBUG:
+            return self.get_response(request)
+
         canonical_host = getattr(settings, "CANONICAL_HOST", None)
         if not canonical_host:
             return self.get_response(request)
