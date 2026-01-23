c529d25d3a14b31c495b5c4cec08f891bf8314ed
Colm Woods <colmwoods13@gmail.com>
2026-01-23 11:42:19 +0000

    Create base.html file and add bolerplate code

--- PCPartsIreland/settings.py
+++ PCPartsIreland/settings.py
@@ -64,7 +64,7 @@ CANONICAL_HOST = "www.pcpartsireland.com"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
