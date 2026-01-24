eb466176525962592b4ee5c2941c070bd472be68
Colm Woods <colmwoods13@gmail.com>
2026-01-24 15:13:36 +0000

    Add STATIC_URL and MEDIA_URL varibles in settings.py

--- PCPartsIreland/settings.py
+++ PCPartsIreland/settings.py
@@ -161,3 +161,13 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_KEY_ID= os.environ.get('AWS_SECRET_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and Media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    MEDIAFILES_LOCATION = 'media'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+    # Override static and media URL's
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
