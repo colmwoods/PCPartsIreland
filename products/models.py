7d9f07b04a629d401f442ce6fd90f1c5eb09ac2b
Colm Woods <colmwoods13@gmail.com>
2026-01-26 19:13:55 +0000

    Change Product model from saving img to /products to saving it at /media

--- products/models.py
+++ products/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)  # optional
-    image = models.ImageField(upload_to="products/", null=True, blank=True)
+    image = models.ImageField(upload_to="media/", null=True, blank=True)
 
 
     def __str__(self):
