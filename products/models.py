b6d28b40ac610a4eca7a447042124b70ce4ab569
Colm Woods <colmwoods13@gmail.com>
2026-01-26 11:17:35 +0000

    Create my category model in products/models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254, unique=True)
+    friendly_name = models.CharField(max_length=254, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name if self.friendly_name else self.name
