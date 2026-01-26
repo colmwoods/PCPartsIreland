e907f78cc6629c5d63313e88dd602a714e13f7e9
Colm Woods <colmwoods13@gmail.com>
2026-01-26 11:19:58 +0000

    Add both models to admin.py so can access them from /admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Product, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name', 'name')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'rating')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
