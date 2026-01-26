5729792911a8a3658c0c7448bf2ae3a6f93dade3
Colm Woods <colmwoods13@gmail.com>
2026-01-26 11:19:17 +0000

    CReate my product model in products/models.py

--- products/models.py
+++ products/models.py
@@ -16,3 +16,17 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name if self.friendly_name else self.name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, null=True, blank=True, on_delete=models.SET_NULL, related_name='products'
+    )
+    name = models.CharField(max_length=254)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)  # optional
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
