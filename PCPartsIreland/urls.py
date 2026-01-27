3e8b9add7d43f8170afe508819442ed3f3f040af
Colm Woods <colmwoods13@gmail.com>
2026-01-27 21:45:20 +0000

    Add profiles.html from boutique_ado and change to suit

--- PCPartsIreland/urls.py
+++ PCPartsIreland/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
