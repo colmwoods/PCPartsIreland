2840432a3661d283b8afa51919e6c8e572de6ff2
Colm Woods <colmwoods13@gmail.com>
2026-01-27 12:08:23 +0000

    Add checkout app and add basic code to checkout.html

--- PCPartsIreland/urls.py
+++ PCPartsIreland/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
