9b541aa592b9fce0031f33b9d87ee45838a9cd51
Colm Woods <colmwoods13@gmail.com>
2026-01-28 11:35:51 +0000

    Removed the country varible from stripe_elements.js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -86,7 +86,6 @@ form.addEventListener('submit', function (ev) {
                         line1: $.trim(form.street_address1.value),
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
-                        country: $.trim(form.country.value),
                         state: $.trim(form.county.value),
                     }
                 }
@@ -98,7 +97,6 @@ form.addEventListener('submit', function (ev) {
                     line1: $.trim(form.street_address1.value),
                     line2: $.trim(form.street_address2.value),
                     city: $.trim(form.town_or_city.value),
-                    country: $.trim(form.country.value),
                     postal_code: $.trim(form.postcode.value),
                     state: $.trim(form.county.value),
                 }
