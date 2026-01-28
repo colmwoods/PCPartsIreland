66351422556dd0a7eb32bcb988692270c8812425
Colm Woods <colmwoods13@gmail.com>
2026-01-28 11:17:31 +0000

    Remove country field from both checkout_success and view.checkout_success

--- checkout/views.py
+++ checkout/views.py
@@ -45,7 +45,6 @@ def checkout(request):
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
             'phone_number': request.POST['phone_number'],
-            'country': request.POST['country'],
             'postcode': request.POST['postcode'],
             'town_or_city': request.POST['town_or_city'],
             'street_address1': request.POST['street_address1'],
@@ -161,7 +160,6 @@ def checkout_success(request, order_number):
         if save_info:
             profile_data = {
                 'default_phone_number': order.phone_number,
-                'default_country': order.country,
                 'default_postcode': order.postcode,
                 'default_town_or_city': order.town_or_city,
                 'default_street_address1': order.street_address1,
