5a344aef1f5a40548ce78055f808999b13a6c480
Colm Woods <colmwoods13@gmail.com>
2026-02-09 12:51:49 +0000

    Remove img function as i am not downloading img as already have on s3 bucket

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-from import_export import resources, fields
-from django.core.files.base import ContentFile
-import requests
+from import_export import resources
 
 from .models import Product, Category
 
@@ -13,8 +11,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 class ProductResource(resources.ModelResource):
-    image_url = fields.Field(column_name='image_url')
-
     class Meta:
         model = Product
         import_id_fields = ('sku',)
@@ -26,25 +22,6 @@ class ProductResource(resources.ModelResource):
             'rating',
             'image_url',
         )
-
-    def dehydrate_image_url(self, obj):
-        if obj.image:
-            return obj.image.url
-        return ''
-
-    def before_import_row(self, row, **kwargs):
-        image_url = row.get('image_url')
-
-        if image_url:
-            try:
-                response = requests.get(image_url, timeout=10)
-                response.raise_for_status()
-
-                filename = image_url.split('/')[-1]
-                row['image'] = ContentFile(response.content, name=filename)
-
-            except Exception as e:
-                print(f"Image download failed: {image_url} ({e})")
 
 
 @admin.register(Product)
