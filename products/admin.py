367933a54d278f32fd6c4f14a419cd213fe18546
Colm Woods <colmwoods13@gmail.com>
2026-02-09 12:37:46 +0000

    Add import and export fields to admin.py now can import and export products

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-from import_export import resources
+from import_export import resources, fields
+from django.core.files.base import ContentFile
+import requests
+
 from .models import Product, Category
-# Register your models here.
 
 
 @admin.register(Category)
@@ -10,8 +12,39 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('friendly_name', 'name')
 
 
+class ProductResource(resources.ModelResource):
+    image_url = fields.Field(column_name='image_url')
+
+    class Meta:
+        model = Product
+        import_id_fields = ('sku',)
+        fields = (
+            'sku',
+            'name',
+            'price',
+            'category',
+            'rating',
+            'image_url',
+        )
+
+    def before_import_row(self, row, **kwargs):
+        image_url = row.get('image_url')
+
+        if image_url:
+            try:
+                response = requests.get(image_url, timeout=10)
+                response.raise_for_status()
+
+                filename = image_url.split('/')[-1]
+                row['image'] = ContentFile(response.content, name=filename)
+
+            except Exception as e:
+                print(f"Image download failed: {image_url} ({e})")
+
+
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ImportExportModelAdmin):
+    resource_class = ProductResource
     list_display = ('name', 'category', 'price', 'rating')
     list_filter = ('category',)
     search_fields = ('name', 'description')
