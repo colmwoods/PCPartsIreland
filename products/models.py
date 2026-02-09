df9c64685c01d362414ed248e71f313d2020e79c
Colm Woods <colmwoods13@gmail.com>
2026-02-09 12:57:47 +0000

    Add image_url to Products model so i can add image_url in as an import

--- products/models.py
+++ products/models.py
@@ -28,6 +28,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)  # optional
     image = models.ImageField(upload_to="media/", null=True, blank=True)
+    image_url = models.URLField(max_length=500, null=True, blank=True)
 
 
     def __str__(self):
