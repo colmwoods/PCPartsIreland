6f50a3c87b80f5589a7df440aaf5790cd98a9ae5
Colm Woods <colmwoods13@gmail.com>
2026-02-09 12:32:01 +0000

    Install django-import-export for admin.py so i can update price files etc

--- PCPartsIreland/settings.py
+++ PCPartsIreland/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "django_extensions",
+    "import_export",
 
     # Allauth
     "allauth",
--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 from .models import Product, Category
+# Register your models here.
 
 
 @admin.register(Category)
