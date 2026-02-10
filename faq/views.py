917df7953bfc882668110c1f50262b580f9ea9e0
Colm Woods <colmwoods13@gmail.com>
2026-02-10 11:02:56 +0000

    Create a faq view

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import FAQ
 
 # Create your views here.
+def faq_list(request):
+    faqs = FAQ.objects.filter(is_active=True)
+    return render(request, "faq/faq.html", {"faqs": faqs})
