791ae1209365c5b72a8a0c64d2c1aab78a3fc82a
Colm Woods <colmwoods13@gmail.com>
2026-02-10 11:00:44 +0000

    Create a FAQ model and add it to admin.py aswell for access

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    is_active = models.BooleanField(default=True)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["order"]
+
+    def __str__(self):
+        return self.question
