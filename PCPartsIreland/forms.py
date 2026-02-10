30ded9b0719029dd23facde57aaf6fe825674611
Colm Woods <colmwoods13@gmail.com>
2026-02-10 10:03:57 +0000

    Create forms.py and take form from gitshowcase.dev

---
+++ PCPartsIreland/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import ContactMessage
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactMessage
+        fields = ['first_name', 'last_name', 'email', 'phone', 'message']
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Enter your first name'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Enter your last name'
+            }),
+            'email': forms.EmailInput(attrs={
+                'placeholder': 'Enter your email',
+                # Requires Full Domain (.com, .net, etc.)
+                'pattern': r'^[^@]+@[^@]+\.[a-zA-Z]{2,}$',
+                'title': (
+                    'Please include a full email domain, '
+                    'like example.com or example.net'
+                ),
+            }),
+            'phone': forms.TextInput(attrs={
+                'placeholder': 'Optional phone number'
+            }),
+            'message': forms.Textarea(attrs={
+                'placeholder': 'Write your message here...'
+            }),
+        }
