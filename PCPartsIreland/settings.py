952762c9f1907b7917de727bb8ce08f83378f253
Colm Woods <colmwoods13@gmail.com>
2026-02-10 14:06:03 +0000

    Make gmail work, by getting app password add to env.py and env vars and call it in settings.py

--- PCPartsIreland/settings.py
+++ PCPartsIreland/settings.py
@@ -193,8 +193,17 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-DEFAULT_FROM_EMAIL = "noreply@pcpartsireland.com"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = "PC Parts Ireland <colmwoods13@gmail.com>"
+
 
 if 'USE_AWS' in os.environ:
     # Cache control
