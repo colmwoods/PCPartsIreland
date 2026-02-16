545d6838cb99a5639a9e8c6b36feb519bfb8774c
Colm Woods <colmwoods13@gmail.com>
2026-02-16 17:38:41 +0000

    Add slug support to Category model and implement SEO-friendly category URLs

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('category/<slug:slug>/', views.category_view, name='category'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('search/', views.product_search, name='product_search'),
     path('search-suggestions/', views.search_suggestions, name='search_suggestions'),
--- products/views.py
+++ products/views.py
@@ -124,3 +124,13 @@ def search_suggestions(request):
     return JsonResponse(data, safe=False)
 
 
+def category_view(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = Product.objects.filter(category=category)
+
+    context = {
+        'products': products,
+        'current_categories': [category],
+    }
+
+    return render(request, 'products/products.html', context)
