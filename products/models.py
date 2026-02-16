1fb434851d584f82b69d588403b7d67cf7b27b6e
Colm Woods <colmwoods13@gmail.com>
2026-02-16 17:03:36 +0000

    Add unique slug field to Category model and populate existing records

--- products/models.py
+++ products/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
-from django.db import models
+
 
 
 class Category(models.Model):
     name = models.CharField(max_length=254, unique=True)
     friendly_name = models.CharField(max_length=254, blank=True, null=True)
+    slug = models.SlugField(max_length=255, unique=True)
 
     # Seo Fields
     meta_title = models.CharField(max_length=255, blank=True, null=True)
@@ -18,6 +19,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def get_friendly_name(self):
         return self.friendly_name if self.friendly_name else self.name
