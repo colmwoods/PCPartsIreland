692196529718a163a62bdcceaff74e441a81d460
Colm Woods <colmwoods13@gmail.com>
2026-02-17 16:48:06 +0000

    Add unit test for home page view

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client, override_settings
+from django.urls import reverse
 
 # Create your tests here.
+@override_settings(SECURE_SSL_REDIRECT=False)
+class HomeViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse("home")
+
+    def test_home_page_loads(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/index.html")
