0b9ca29fd823ec7a71a62a6c32594149ce46f8ad
Colm Woods <colmwoods13@gmail.com>
2026-02-18 16:39:25 +0000

    Add the 3 views into urls.py

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('add/', views.add_product, name='add_product'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('<int:product_id>/edit/', views.edit_product, name='edit_product'),
+    path('<int:product_id>/delete/', views.delete_product, name='delete_product'),
     path('search/', views.product_search, name='product_search'),
     path('search-suggestions/', views.search_suggestions, name='search_suggestions'),
 ]
