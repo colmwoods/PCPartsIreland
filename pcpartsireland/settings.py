ded970293f92cae4f92377d51638264a71f7da66
Colm Woods <colmwoods13@gmail.com>
2026-02-18 17:09:38 +0000

    Debug = TRue

--- pcpartsireland/settings.py
+++ pcpartsireland/settings.py
@@ -31,7 +31,7 @@ if env_path.exists():
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com", "pcpartsireland.com", "www.pcpartsireland.com"]
 
 
