ad1d289fb6838fe20b52d20e55b5f92b2edbfbf3
Colm Woods <colmwoods13@gmail.com>
2026-03-02 11:49:55 +0000

    Add python unit testing for about.html in home/tests.py

--- home/tests.py
+++ home/tests.py
@@ -9,9 +9,15 @@ class HomeViewTests(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.url = reverse("home")
+        self.home_url = reverse("home")
+        self.about_url = reverse("about")
 
     def test_home_page_loads(self):
-        response = self.client.get(self.url)
+        response = self.client.get(self.home_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home/index.html")
+
+    def test_about_page_loads(self):
+        response = self.client.get(self.about_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/about.html")
