5d8fbf5b84629b1ef3ee221ae2637aafc7028022
Colm Woods <colmwoods13@gmail.com>
2026-03-22 22:30:30 +0000

    Add orders field in /admin to view orders

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from .models import Order, OrderLineItem
 # Register your models here.
+
+class OrderLineItemInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('product', 'quantity', 'lineitem_total')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemInline,)
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+        'original_bag',
+        'stripe_pid',
+    )
+
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'email',
+        'grand_total',
+    )
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
